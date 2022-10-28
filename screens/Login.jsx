7481eda91ff065b4e3f1e54564b0cbd605edc01b
v1ab1 <sharkandmouse@gmail.com>
2022-10-28 13:52:38 +0500

    work on layout

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,5 +1,5 @@
 import { useState} from 'react';
-import { StyleSheet, View, ImageBackground, Text, Button } from 'react-native';
+import { StyleSheet, View, ImageBackground, Text, TouchableHighlight } from 'react-native';
 
 const background = '../assets/login-background.png';
 
@@ -7,10 +7,14 @@ export const Login = () => (
     <View style={styles.container}>
       <ImageBackground source={require(background)} resizeMode="cover" style={styles.image}>
         <View>
-            <Text style={styles.text}>Welcome</Text>
+            <Text style={styles.text}>Добро пожаловать</Text>
         </View>
         <View style={styles.loginWrapper}>
-            <Button style={styles.button} title="Welcome to NFT World" />
+            <TouchableHighlight style={styles.button} underlayColor="white">
+                <Text style={styles.buttonText}>
+                    Погрузимся в мир NFT
+                </Text>
+            </TouchableHighlight>
         </View>
       </ImageBackground>
     </View>
@@ -37,14 +41,26 @@ export const Login = () => (
     button: {
         backgroundColor: "rgba(151, 169, 246, 1)",
         color: "white",
-        padding: "15px 30px",
-        gap: "10px",
-        borderRadius: "30px",
-        fontSize: 15
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        borderRadius: 30,
+        fontSize: 15,
+        marginVertical: 40,
+        marginHorizontal: 68,
+    },
+    buttonText: {
+        color: "white",
+        letterSpacing: -0.36,
+        fontWeight: 600,
+        fontSize: 16,
+        lineHeight: 20
     },
     loginWrapper: {
         backgroundColor: "rgba(89, 70, 119, 1)",
         BackgroundBlendMode: "overlay",
-        backdropFilter: "blur"
+        backdropFilter: "blur",
+        marginVertical: 150,
+        alignItems: 'center',
+        alignSelf: 'center'
     }
   });
