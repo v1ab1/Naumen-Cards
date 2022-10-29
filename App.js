513453a5399e789b59005488bf844fa9b3b3a2f0
ven <venchass@ayndex.ru>
2022-10-29 13:17:13 +0500

    test

--- App.js
+++
@@ -1,7 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Navigation } from './screens/Navigation';
-
-export default function App() {
-  return <Navigation />;
-}
