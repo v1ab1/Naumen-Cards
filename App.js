f1c932c3ba260ba111ece3456ade1d53e3302bf0
venchass <venchass@ayndex.ru>
2022-10-29 13:52:56 +0500

    add screens

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Navigation } from './screens/Navigation';
-//kekw
+
 export default function App() {
   return <Navigation />;
 }
