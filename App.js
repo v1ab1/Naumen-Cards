6e96793786faec753a2e08a19de2190706266c81
v1ab1 <sharkandmouse@gmail.com>
2022-10-29 13:21:27 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Navigation } from './screens/Navigation';
 
 export default function App() {
