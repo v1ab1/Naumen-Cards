8bc848eefad12bc8feea6f5c8101f567bdee9a00
v1ab1 <sharkandmouse@gmail.com>
2022-10-29 14:39:11 +0500

    navbar and market created

---
+++ components/NavBar.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, View, ImageBackground, Text, TouchableHighlight, KeyboardAvoidingView } from 'react-native';
+
+export const NavBar = () => {
+    return (
+        <View>
+            <Text>1</Text>
+        </View>
+    );
+};
