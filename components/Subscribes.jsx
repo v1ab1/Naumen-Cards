cf4da7b7cec17773bbb8d02be4721cd4fedecd83
venchass <venchass@ayndex.ru>
2022-10-30 14:30:40 +0500

    update

--- components/Subscribes.jsx
+++ components/Subscribes.jsx
@@ -1,5 +1,5 @@
 import { View,Text,TouchableOpacity, StyleSheet } from "react-native";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../Context";
 import { SubscribeItem } from "./SubscribeItem";
 import { FlatList, TextInput } from "react-native-gesture-handler";
@@ -7,44 +7,99 @@ import { Follower } from "./Follower";
 import axios from "axios";
 import { baseUrl } from "../baseUrl";
 import { SelectedProfile } from "./SelectedProfile";
+
+class SubscribesClass extends React.Component{
+
+    componentDidMount(){
+        axios.get(`${baseUrl}/get_friends?login=${this.props.context.login}`)
+        .then((res) => this.setState({subscribes : res.data}));
+        
+    }
+
+
+    render(){
+        let props = this.props;
+        let Subscribe = (from, to, callBack) => {
+            axios.post(`${baseUrl}/add_friend?login=${from}&friend=${to}`)
+            .then(() => {});
+        }
+        return (
+            <View>
+                {props.isViewUserProfile ? <SelectedProfile setisViewUserProfile={props.setisViewUserProfile} selectedProfile={props.selectedProfile}/> 
+                :
+                <View>
+                    <View style={styles.formWrap}>
+                        <TextInput
+                            onChangeText={props.setLogin}
+                            value={props.login}
+                            placeholder="Логин"
+                            placeholderTextColor="rgba(255,255,255,0.7)"
+                            style={styles.form}
+                            />
+                        <TouchableOpacity style={styles.formButton} onPress={() => {Subscribe(props.context.login, props.login)}}>
+                            <Text style={styles.formText}>Подписаться</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <FlatList
+                    data={this?.state?.subscribes ? this?.state?.subscribes : []}
+                    renderItem={({item}) => <Follower setisViewUserProfile={props.setisViewUserProfile} setSelectedProfile={props.setSelectedProfile}  item={item}/>}
+                    />
+                </View>}
+            </View>
+        )
+    }
+}
+
 export const Subscribes = (props) => {
     const [context, setContext] = useContext(Context);
     const [login, setLogin] = useState("");
-
     const [isViewUserProfile, setisViewUserProfile] = useState(false);
     const [selectedProfile, setSelectedProfile] = useState();
 
 
-    let Subscribe = (from, to, callBack) => {
-        axios.post(`${baseUrl}/add_friend?login=${from}&friend=${to}`)
-        .then(() => {});
-    }
-
-    return (
-        <View>
-            {isViewUserProfile ? <SelectedProfile setisViewUserProfile={setisViewUserProfile} selectedProfile={selectedProfile}/> 
-            :
-            <View>
-                <View style={styles.formWrap}>
-                    <TextInput
-                        onChangeText={setLogin}
-                        value={login}
-                        placeholder="Логин"
-                        placeholderTextColor="rgba(255,255,255,0.7)"
-                        style={styles.form}
-                        />
-                    <TouchableOpacity style={styles.formButton} onPress={() => {Subscribe(context.login, login)}}>
-                        <Text style={styles.formText}>Подписаться</Text>
-                    </TouchableOpacity>
-                </View>
-                <FlatList
-                data={props.subscribes}
-                renderItem={({item}) => <Follower setisViewUserProfile={setisViewUserProfile} setSelectedProfile={setSelectedProfile}  item={item}/>}
-                />
-            </View>}
-        </View>
-    )
+    return ( <SubscribesClass {...props} context={context} setContext={setContext} login={login} setLogin={setLogin} isViewUserProfile={isViewUserProfile}
+        setisViewUserProfile={setisViewUserProfile} selectedProfile={selectedProfile} setSelectedProfile={setSelectedProfile}/>)
 }
+
+
+// export const Subscribes = (props) => {
+//     const [context, setContext] = useContext(Context);
+//     const [login, setLogin] = useState("");
+
+//     const [isViewUserProfile, setisViewUserProfile] = useState(false);
+//     const [selectedProfile, setSelectedProfile] = useState();
+
+
+//     let Subscribe = (from, to, callBack) => {
+//         axios.post(`${baseUrl}/add_friend?login=${from}&friend=${to}`)
+//         .then(() => {});
+//     }
+
+//     return (
+//         <View>
+//             {isViewUserProfile ? <SelectedProfile setisViewUserProfile={setisViewUserProfile} selectedProfile={selectedProfile}/> 
+//             :
+//             <View>
+//                 <View style={styles.formWrap}>
+//                     <TextInput
+//                         onChangeText={setLogin}
+//                         value={login}
+//                         placeholder="Логин"
+//                         placeholderTextColor="rgba(255,255,255,0.7)"
+//                         style={styles.form}
+//                         />
+//                     <TouchableOpacity style={styles.formButton} onPress={() => {Subscribe(context.login, login)}}>
+//                         <Text style={styles.formText}>Подписаться</Text>
+//                     </TouchableOpacity>
+//                 </View>
+//                 <FlatList
+//                 data={props.subscribes}
+//                 renderItem={({item}) => <Follower setisViewUserProfile={setisViewUserProfile} setSelectedProfile={setSelectedProfile}  item={item}/>}
+//                 />
+//             </View>}
+//         </View>
+//     )
+// }
 
 const styles = StyleSheet.create({
     cardWrapper: {
