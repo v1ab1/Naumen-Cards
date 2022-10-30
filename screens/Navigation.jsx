3a9ea0356ef0e578efff4c54392a365fefca8077
v1ab1 <sharkandmouse@gmail.com>
2022-10-30 11:08:34 +0500

    Update Navigation.jsx

--- screens/Navigation.jsx
+++ screens/Navigation.jsx
@@ -1,25 +1,28 @@
+import { Context } from "../Context";
+import { useContext } from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-
 import { Login } from './Login';
 import { Profile } from './Profile';
 import { AddingItem } from './AddingItem';
 import { Market } from './Market';
 import { TopBar } from '../components/TopBar';
+import { NavBar } from '../components/NavBar';
 const Stack = createNativeStackNavigator();
 
 
 
 export const Navigation = () => {
+    const [context, setContext] = useContext(Context);
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} options={{headerShown: false }} />
+          {context.isLogin ? null : <Stack.Screen name="Login" component={Login} options={{headerShown: false }} />}
           <Stack.Screen name="Profile" component={Profile} options={{headerShown: false }} />
           <Stack.Screen name="AddingItem" component={AddingItem} options={{headerShown: false }} />
           <Stack.Screen name="Market" component={Market} options={{headerShown: false }} />
         </Stack.Navigator>
-        <TopBar/>
+        {context.isLogin ? <TopBar/> : null}
       </NavigationContainer>
     );
 };
