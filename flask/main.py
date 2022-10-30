c5c055b0212f2bee1cb63b367e503de0f8852231
Ramil Mavlutov <stirk2281337@gmail.com>
2022-10-30 08:37:52 +0500

    update server

--- flask/main.py
+++ flask/main.py
@@ -1,12 +1,15 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_file
+from flask import Flask, request, send_file
 from werkzeug.utils import secure_filename
 import json
+from heic2png import HEIC2PNG
+from PIL import Image
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'heic'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/img'
+IMG_FOLDER = '/static/img'
+app.config['UPLOAD_FOLDER'] = './static/img'
 
 
 def allowed_file(filename):
@@ -14,37 +17,70 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+def open_json(filename):
+    with open("static/" + filename, "r") as jsonFile:
+        data = json.load(jsonFile)
+    return data
+
+
+def write_json(data, filename):
+    with open("static/" + filename, "w") as jsonFile:
+        json.dump(data, jsonFile)
+
+
+def find_id(data, id, name):
+    for i in range(len(data[name])):
+        if data[name][i]['id'] == int(id):
+            item = data[name][i]
+            del data[name][i]
+            return item, data
+    return "404", "404"
+
+
+def upload_photo(file, name):
+    start = file.filename.split('.')[0]
+    ending = file.filename.split('.')[1].lower()
+    file.filename = start + "." + ending
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        end = filename.split('.')[1]
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(name)) + "." + end)
+        if end == 'heic':
+            heic_img = HEIC2PNG('static/img/' + str(name) + "." + end)
+            heic_img.save()
+            os.remove("static/img/" + str(name) + "." + end)
+            end = 'png'
+        foo = Image.open('static/img/' + str(name) + "." + end)
+        if end == 'png':
+            foo = foo.convert('RGB')
+        foo.save('static/img/' + str(name) + ".jpg", optimize=True, quality=50)
+        os.remove('static/img/' + str(name) + '.png')
 
 
 @app.route('/register', methods=['POST'])
 def register():
     login = request.args['login']
     password = request.args['password']
-    with open("static/users.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    # telegram = request.args['telegram']
+    data = open_json("users.json")
     if login in data:
         return "409"
     data[login] = password
-    with open("static/users.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
-    with open("static/items.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    write_json(data, "users.json")
+    data = open_json("items.json")
     data[login] = []
-    with open("static/items.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
-    with open("static/coins.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    write_json(data, "items.json")
+    data = open_json("coins.json")
     data[login] = 1000
-    with open("static/coins.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
-    with open("static/friends.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    write_json(data, "coins.json")
+    data = open_json("friends.json")
     data[login] = []
-    with open("static/friends.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    write_json(data, "friends.json")
+    # data = open_json("telegrams.json")
+    # data[login] = telegram
+    # write_json(data, "telegrams.json")
+    file = request.files['file']
+    upload_photo(file, login)
     return "200"
 
 
@@ -59,8 +95,7 @@ def get_market():
 def login():
     login = request.args['login']
     password = request.args['password']
-    with open("static/users.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    data = open_json("users.json")
     if login in data:
         if data[login] == password:
             return "200"
@@ -72,25 +107,22 @@ def upload_item():
     login = request.args['login']
     name = request.args['name']
     description = request.args['description']
-    #picture
-    with open("static/items.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    data = open_json("items.json")
     with open("static/id.txt", "r") as txtFile:
         id = int(txtFile.readline())
-    with open("static/coins.json", "r") as jsonFile:
-        data_coins = json.load(jsonFile)
+    data_coins = open_json("coins.json")
     data_coins[login] -= 1
     id += 1
     item = {"id": id,
             "title": name,
             "description": description}
     data[login].append(item)
-    with open("static/items.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    write_json(data, "items.json")
     with open("static/id.txt", "w") as txtFile:
         txtFile.write(str(id))
-    with open("static/coins.json", "w") as jsonFile:
-        json.dump(data_coins, jsonFile)
+    write_json(data_coins, "coins.json")
+    file = request.files['file']
+    upload_photo(file, id)
     return "200"
 
 
@@ -98,21 +130,11 @@ def upload_item():
 def buy_item():
     login = request.args['login']
     id = request.args['id']
-    with open("static/market.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-    with open("static/coins.json", "r") as jsonFile:
-        data_coins = json.load(jsonFile)
-    with open("static/items.json", "r") as jsonFile:
-        data_items = json.load(jsonFile)
-    found = False
-    item = {}
-    for i in range(len(data['market'])):
-        if data['market'][i]['id'] == int(id):
-            item = data['market'][i]
-            found = True
-            del data['market'][i]
-            break
-    if not found:
+    data = open_json("market.json")
+    data_coins = open_json("coins.json")
+    data_items = open_json("items.json")
+    item, data = find_id(data, id, 'market')
+    if item == "404":
         return "404"
     if int(data_coins[login]) >= int(item['price']):
         data_coins[login] = int(data_coins[login]) - int(item['price'])
@@ -120,12 +142,9 @@ def buy_item():
         del item['owner']
         del item['price']
         data_items[login].append(item)
-        with open("static/market.json", "w") as jsonFile:
-            json.dump(data, jsonFile)
-        with open("static/coins.json", "w") as jsonFile:
-            json.dump(data_coins, jsonFile)
-        with open("static/items.json", "w") as jsonFile:
-            json.dump(data_items, jsonFile)
+        write_json(data, "market.json")
+        write_json(data_coins, "coins.json")
+        write_json(data_items, "items.json")
         return "200"
     else:
         return "403"
@@ -136,35 +155,23 @@ def sell_item():
     login = request.args['login']
     id = request.args['id']
     price = request.args['price']
-    with open("static/items.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-    found = False
-    item = {}
-    for i in range(len(data[login])):
-        if data[login][i]['id'] == int(id):
-            item = data[login][i]
-            found = True
-            del data[login][i]
-            break
-    if not found:
+    data = open_json("items.json")
+    item, data = find_id(data, id, login)
+    if item == "404":
         return "404"
     item['owner'] = login
     item['price'] = price
-    with open("static/market.json", "r") as jsonFile:
-        data_market = json.load(jsonFile)
+    data_market = open_json("market.json")
     data_market['market'].append(item)
-    with open("static/market.json", "w") as jsonFile:
-        json.dump(data_market, jsonFile)
-    with open("static/items.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    write_json(data_market, "market.json")
+    write_json(data, "items.json")
     return "200"
 
 
 @app.route('/get_friends', methods=['GET'])
 def get_friends():
     login = request.args['login']
-    with open("static/friends.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    data = open_json("friends.json")
     return json.dumps(data[login])
 
 
@@ -172,30 +179,18 @@ def get_friends():
 def return_item():
     login = request.args['login']
     id = request.args['id']
-    with open("static/market.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-    found = False
-    item = {}
-    for i in range(len(data['market'])):
-        if data['market'][i]['id'] == int(id):
-            if data['market'][i]['owner'] == login:
-                item = data['market'][i]
-                found = True
-                del data['market'][i]
-                break
-            else:
-                return "403"
-    if not found:
+    data = open_json("market.json")
+    item, data = find_id(data, id, 'market')
+    if item == "404":
         return "404"
-    with open("static/items.json", "r") as jsonFile:
-        data_items = json.load(jsonFile)
+    if item['owner'] != login:
+        return "403"
+    data_items = open_json("items.json")
     del item['owner']
     del item['price']
     data_items[login].append(item)
-    with open("static/market.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
-    with open("static/items.json", "w") as jsonFile:
-        json.dump(data_items, jsonFile)
+    write_json(data, "market.json")
+    write_json(data_items, "items.json")
     return "200"
 
 
@@ -204,23 +199,14 @@ def send_item():
     login = request.args['login']
     id = request.args['id']
     to = request.args['to']
-    with open("static/items.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    data = open_json("items.json")
     if to not in data:
         return "404"
-    found = False
-    item = ['None']
-    for i in range(len(data[login])):
-        if data[login][i]['id'] == int(id):
-            item = data[login][i]
-            found = True
-            del data[login][i]
-            break
-    if not found:
+    item, data = find_id(data, id, login)
+    if item == "404":
         return "404"
     data[to].append(item)
-    with open("static/items.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    write_json(data, "items.json")
     return "200"
 
 
@@ -236,24 +222,21 @@ def get_items():
 def add_friend():
     login = request.args['login']
     friend = request.args['friend']
-    with open("static/friends.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-    with open("static/users.json", "r") as jsonFile:
-        data_users = json.load(jsonFile)
+    data = open_json("friends.json")
+    data_users = open_json("users.json")
     if friend not in data_users:
         return "404"
     if friend in data[login]:
         return "403"
     data[login].append(friend)
-    with open("static/friends.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    write_json(data, "friends.json")
     return "200"
+
 
 @app.route('/get_coins', methods=['GET'])
 def get_coins():
     login = request.args['login']
-    with open("static/coins.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    data = open_json("coins.json")
     return json.dumps(data[login])
 
 
@@ -261,50 +244,20 @@ def get_coins():
 def delete_friend():
     login = request.args['login']
     friend = request.args['friend']
-    with open("static/friends.json", "r") as jsonFile:
-        data = json.load(jsonFile)
+    data = open_json("friends.json")
     if friend not in data[login]:
         return "404"
     else:
         index = data[login].index(friend)
         del data[login][index]
-    with open("static/friends.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    write_json(data, "friends.json")
     return "200"
+
 
 @app.route('/get_image', methods=['GET'])
 def get_image():
-    folder = request.args['object']
-
-    return send_file("static/img/logitech.jpg")
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "200"
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    name = request.args['name']
+    return send_file("static/img/" + name + ".jpg")
 
 
 app.run(host="172.20.10.9")
